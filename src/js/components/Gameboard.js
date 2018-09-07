ac9cb6c1e16451d553e3e873a3935600dda1e830
Misha Herscu <mishaherscu@gmail.com>
2018-09-07 01:35:19 -0400

    made the board a bit taller

--- src/js/components/Gameboard.js
+++ src/js/components/Gameboard.js
@@ -13,21 +13,29 @@ export default class Gameboard extends React.Component {
   }
 
   render() {
+
+    const trStyle = {
+      height: '180px',
+      fontSize: '90px',
+      fontFamily: 'sans-serif',
+      fontWeight: 400
+    };
+
     return (
       <div class="container">
         <table>
           <tbody>
-            <tr class="row">
+            <tr class="row" style={trStyle}>
               <td id="cell-0" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[0]}</td>
               <td id="cell-1" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[1]}</td>
               <td id="cell-2" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[2]}</td>
             </tr>
-            <tr class="row">
+            <tr class="row" style={trStyle}>
               <td id="cell-3" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[3]}</td>
               <td id="cell-4" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[4]}</td>
               <td id="cell-5" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[5]}</td>
             </tr>
-            <tr class="row">
+            <tr class="row" style={trStyle}>
               <td id="cell-6" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[6]}</td>
               <td id="cell-7" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[7]}</td>
               <td id="cell-8" class="col-xs-4" onClick={this.handleCellClick.bind(this)}>{this.props.boardVals[8]}</td>
